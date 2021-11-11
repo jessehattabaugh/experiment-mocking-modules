de295159ea09f61eb09d8a634b5ac28406f28a6e
jhattabaugh@autofi.io <arkanciscan@gmail.com>
2021-11-11 12:21:59 -0800

    using isolateModules

--- sum.test.js
+++ sum.test.js
@@ -1,15 +1,20 @@
 const sum = require("./sum");
 
+let config;
+jest.isolateModules(() => {
+  config = require("./config");
+});
+
 test("without a mock", () => {
   expect(sum(1, 2)).toBe(85);
 });
 
 test("with a mock of 1", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
+  config = { FOO: { BAR: 1 } };
   expect(sum(1, 2)).toBe(3);
 });
 
 test("with a different mock", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
+  config = { FOO: { BAR: 6 } };
   expect(sum(1, 2)).toBe(18);
 });
