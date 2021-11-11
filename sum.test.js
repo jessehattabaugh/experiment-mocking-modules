6777497d5cc9b652f1c6fc87f4ad70a43181bd97
jhattabaugh@autofi.io <arkanciscan@gmail.com>
2021-11-11 12:04:37 -0800

    better tests

--- sum.test.js
+++ sum.test.js
@@ -1,15 +1,15 @@
 const sum = require("./sum");
 
 test("without a mock", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(85);
 });
 
-test("with a mock of 6", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
-  expect(sum(1, 2)).toBe(18);
+test("with a mock of 1", () => {
+  jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
+  expect(sum(1, 2)).toBe(3);
 });
 
 test("with a different mock", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
-  expect(sum(1, 2)).toBe(3);
+  jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
+  expect(sum(1, 2)).toBe(18);
 });
