872b684f1bc4580d94afcb21b179c7e2865605e8
jhattabaugh@autofi.io <arkanciscan@gmail.com>
2021-11-11 12:29:00 -0800

    simply mutating the mocked module

--- sum.test.js
+++ sum.test.js
@@ -1,15 +1,26 @@
+jest.mock("./config.js");
+
 const sum = require("./sum");
+const config = require("./config");
+
+afterEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+  jest.restoreAllMocks();
+});
 
 test("without a mock", () => {
   expect(sum(1, 2)).toBe(85);
 });
 
 test("with a mock of 1", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
+  // jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
+  config.FOO.BAR = 1;
   expect(sum(1, 2)).toBe(3);
 });
 
 test("with a different mock", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
+  // jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
+  config.FOO.BAR = 6;
   expect(sum(1, 2)).toBe(18);
 });
