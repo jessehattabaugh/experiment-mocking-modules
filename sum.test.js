71914a51374821fa68e5655a43e0f25095bc4ebf
jhattabaugh@autofi.io <arkanciscan@gmail.com>
2021-11-11 14:56:37 -0800

    improvements to base

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,11 @@
 const sum = require("./sum");
 
+afterEach(() => {
+  // jest.clearAllMocks();
+  // jest.resetAllMocks();
+  // jest.restoreAllMocks();
+});
+
 test("without a mock", () => {
   expect(sum(1, 2)).toBe(85);
 });
@@ -11,5 +17,5 @@ test("with a mock of 1", () => {
 
 test("with a different mock", () => {
   jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
-  expect(sum(1, 2)).toBe(18);
+  expect(sum(1, 2)).toBe(13);
 });
