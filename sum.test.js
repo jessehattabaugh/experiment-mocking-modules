8b9c7c7ab1c0553d9ae0eb8faa4906ba6b202c42
jhattabaugh@autofi.io <arkanciscan@gmail.com>
2021-11-11 11:54:54 -0800

    attempting to spyOn default

--- sum.test.js
+++ sum.test.js
@@ -1,15 +1,17 @@
 const sum = require("./sum");
 
+const config = require("./config.js");
+
 test("without a mock", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(85);
 });
 
-test("with a mock of 6", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
-  expect(sum(1, 2)).toBe(18);
+test("with a mock of 1", () => {
+  jest.spyOn(config, "default").mockReturnValue({ FOO: { BAR: 1 } });
+  expect(sum(1, 2)).toBe(3);
 });
 
 test("with a different mock", () => {
-  jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
-  expect(sum(1, 2)).toBe(3);
+  jest.spyOn(config, "default").mockReturnValue({ FOO: { BAR: 6 } });
+  expect(sum(1, 2)).toBe(18);
 });
