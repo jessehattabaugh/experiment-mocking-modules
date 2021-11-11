7565215f0388dfc1dc489658d9c9dd2be7b219e6
jhattabaugh@autofi.io <arkanciscan@gmail.com>
2021-11-11 12:35:06 -0800

    maybe I can just mutate the mocked module?

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 jest.mock("./config.js");
 
 const sum = require("./sum");
-const config = require("./config");
+let config = require("./config");
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -10,17 +10,16 @@ afterEach(() => {
 });
 
 test("without a mock", () => {
+  config = jest.requireActual("./config");
   expect(sum(1, 2)).toBe(85);
 });
 
 test("with a mock of 1", () => {
-  // jest.mock("./config.js", () => ({ FOO: { BAR: 1 } }));
   config.FOO.BAR = 1;
   expect(sum(1, 2)).toBe(3);
 });
 
 test("with a different mock", () => {
-  // jest.mock("./config.js", () => ({ FOO: { BAR: 6 } }));
   config.FOO.BAR = 6;
   expect(sum(1, 2)).toBe(18);
 });
